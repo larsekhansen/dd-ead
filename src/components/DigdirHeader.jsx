a13e93e2db200beca2ff3d3c38c7e6339c9d8677
Lars Eirik Korsgaard Hansen <lars.eirik.korsgaard.hansen@nav.no>
2025-03-28 13:59:56 +0100

    doesnt break

--- src/components/DigdirHeader.jsx
+++ src/components/DigdirHeader.jsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import { Header, HeaderLogo, HeaderMenu, HeaderMenuButton, HeaderMenuGroup, HeaderMenuItem } from '@digdir/designsystemet-react';
+import React from "react";
+import { Link } from "@digdir/designsystemet-react";
+import "../styles/header.css";
 
 export const DigdirHeader = () => {
   return (
-    <Header>
-      <HeaderLogo title="KI Norge" href="/" />
-      <HeaderMenu>
-        <HeaderMenuGroup>
-          <HeaderMenuItem href="/">Forsiden</HeaderMenuItem>
-          <HeaderMenuItem href="/hva-er-ki-norge">Hva er KI Norge?</HeaderMenuItem>
-          <HeaderMenuItem href="/blogg">Blogg</HeaderMenuItem>
-          <HeaderMenuItem href="/om-oss">Om oss</HeaderMenuItem>
-        </HeaderMenuGroup>
-        <HeaderMenuGroup position="right">
-          <HeaderMenuItem href="/admin">Admin</HeaderMenuItem>
-        </HeaderMenuGroup>
-      </HeaderMenu>
-      <HeaderMenuButton />
-    </Header>
+    <header className="site-header">
+      <div className="header-container">
+        <a className="header-logo" href="/" title="KI Norge">
+          KI Norge
+        </a>
+        <nav className="header-nav">
+          <ul className="header-menu">
+            <li>
+              <Link href="/">Forsiden</Link>
+            </li>
+            <li>
+              <Link href="/hva-er-ki-norge">Hva er KI Norge?</Link>
+            </li>
+            <li>
+              <Link href="/blogg">Blogg</Link>
+            </li>
+            <li>
+              <Link href="/om-oss">Om oss</Link>
+            </li>
+          </ul>
+          <ul className="header-menu header-menu-right">
+            <li>
+              <Link href="/admin">Admin</Link>
+            </li>
+          </ul>
+        </nav>
+        <button
+          className="header-menu-button"
+          aria-label="Meny"
+          onClick={() => {
+            const nav = document.querySelector(".header-nav");
+            nav.classList.toggle("open");
+          }}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+    </header>
   );
 };
 
-export default DigdirHeader; 
+export default DigdirHeader;
